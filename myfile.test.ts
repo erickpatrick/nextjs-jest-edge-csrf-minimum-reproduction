6963d526561a8df5275297faeddd0c17435cf534
Erick Patrick <github@erickpatrick.net>
2022-12-13 16:18:17 +0100

    adjust test file

--- myfile.test.ts
+++ myfile.test.ts
@@ -6,10 +6,7 @@ import { exportedFunction } from "./myfile";
 import { NextRequest } from "next/server";
 
 describe('ddd', () => {
-    it('description', async () => {
-        // Arrange
+    it('description of testcase', async () => {
         expect(await exportedFunction({} as NextRequest)).toBe(undefined)
-        // Act
-        // Assert
     });
 })
